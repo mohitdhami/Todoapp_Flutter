806984ae9b21c777b5fa885ab7c5b0845ecc68f3
FlutLab.io <>
2020-12-27 04:10:09 +0000

    Autosave

--- lib/initialRoute.dart
+++ lib/initialRoute.dart
@@ -89,7 +89,7 @@ class _InitialRouteState extends State<InitialRoute> {
                   : ListView.builder(
                       itemCount: listtaskTitle.length,
                       itemBuilder: (context, index) {
-                        final item = index;
+                        final item = listtaskTitle.length-index;
                         return Column(
                           children: [
                             Dismissible(
